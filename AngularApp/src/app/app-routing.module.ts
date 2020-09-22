e918914b30276a51336e3f2c2b760f7d8868fb38
VinnieVenture <57813811+VinnieVenture@users.noreply.github.com>
2020-09-22 08:59:48 +0200

    pipes

--- AngularApp/src/app/app-routing.module.ts
+++ AngularApp/src/app/app-routing.module.ts
@@ -4,6 +4,7 @@ import { MenuComponent } from './components/menu/menu.component';
 import { NgForExampleComponent } from './components/ng-for-example/ng-for-example.component';
 import { NgIfExampleComponent } from './components/ng-if-example/ng-if-example.component';
 import { NgSwitchExampleComponent } from './components/ng-switch-example/ng-switch-example.component';
+import { PipesExampleComponent } from './components/pipes-example/pipes-example.component';
 import { TestComponent } from './components/test/test.component';
 import { Test2Component } from './components/test2/test2.component';
 
@@ -15,6 +16,7 @@ const routes: Routes =
   { path: 'ngForComponent', component: NgForExampleComponent },
   { path: 'ngIfComponent', component: NgIfExampleComponent },
   { path: 'ngSwitchComponent', component: NgSwitchExampleComponent },
+  { path: 'pipes', component: PipesExampleComponent },
   { path: '', redirectTo: './test', pathMatch: 'full'},
   { path: '**', component: TestComponent },
 
